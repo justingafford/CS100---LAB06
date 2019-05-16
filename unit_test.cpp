a3386578bcb9ef7f0a8f766b95d2503c9ef0dc3a
Justin Gafford <jgaff001@ucr.edu>
2019-05-16 13:44:42 -0700

    Update unit_test.cpp

--- unit_test.cpp
+++ unit_test.cpp
@@ -60,7 +60,7 @@ TEST(DecoratorTest,TruncTest) {
      Op* op3 = new Op(4);
      Sub* sub1 = new Sub(op2,op3);
      Trunc* trunc1 = new Trunc(sub1);
-     Add* add1 = new Op(op1,trunc1);
+     Add* add1 = new Add(op1,trunc1);
      EXPECT_EQ(add1->stringify(),"5+3");
      
 }
