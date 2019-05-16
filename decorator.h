80f47a912f792b019a58b324a14183a03558b363
vtsai006 <49217659+vtsai006@users.noreply.github.com>
2019-05-15 22:16:18 -0700

    Create decorator.h

---
+++ decorator.h
@@ -0,0 +1,49 @@
+#include <math.h>
+#inlcude "base.h"
+
+class Decorator : publec Base
+{
+    protected:
+        Base* core;
+
+    public:
+        Decorator(Base* core);
+        virtual double evaluate() = 0;
+        virtual std::string stringify() = 0;
+}
+
+Class Ceil : public Decorator
+{
+    public: 
+        Ceil(Base* core);
+        virtual double evaluate(); 
+}
+
+Class Floor : public Decorator
+{
+    public: 
+        Floor(Base* core);
+        virtual double evaluate(); 
+}
+
+Class Abs : public Decorator
+{
+    public: 
+        Abs(Base* core);
+        virtual double evaluate(); 
+}
+
+Class Trunc : public Decorator
+{
+    public: 
+        Trunc(Base* core);
+        virtual std::string stringify(); 
+}
+
+Class Paren : public Decorator
+{
+    public: 
+        Paren(Base* core);
+        virtual std::string stringify(); 
+}
+
