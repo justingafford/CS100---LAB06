1581781d2631f66f7997caf36eea385bb72a219d
Justin Gafford <jgaff001@ucr.edu>
2019-05-16 11:52:54 -0700

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "BubbleSort.hpp"
 
-void BubbleSort::BubbleSort(Container* container) {
+void BubbleSort::sort(Container* container) {
 	for (unsigned i = 0; i < container->size(); i++) {
 		for (unsigned j = 0; j < container->size()-1; i++) {
 			if((*container).at(j)->evaluate() > (*container).at(j+1)->evaluate()) {
