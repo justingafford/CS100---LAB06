eb0542d3d1d94d3680238801d4cabffbbc613afa
Justin Gafford <jgaff001@ucr.edu>
2019-05-16 11:54:41 -0700

    Update decorator.h

--- decorator.h
+++ decorator.h
@@ -1,3 +1,6 @@
+#ifndef DECORATOR_H
+#define DECORATOR_H
+
 #include <math.h>
 #include "base.hpp"
 
@@ -10,40 +13,41 @@ class Decorator : public Base
         Decorator(Base* core){};
         virtual double evaluate() = 0;
         virtual std::string stringify() = 0;
-}
+};
 
 Class Ceil : public Decorator
 {
     public: 
         Ceil(Base* core) : Decorator(core){};
         virtual double evaluate(); 
-}
+};
 
 Class Floor : public Decorator
 {
     public: 
         Floor(Base* core) : Decorator(core){};
         virtual double evaluate(); 
-}
+};
 
 Class Abs : public Decorator
 {
     public: 
         Abs(Base* core) : Decorator(core){};
         virtual double evaluate(); 
-}
+};
 
 Class Trunc : public Decorator
 {
     public: 
         Trunc(Base* core) : Decorator(core){};
         virtual std::string stringify(); 
-}
+};
 
 Class Paren : public Decorator
 {
     public: 
         Paren(Base* core) : Decorator(core){};
         virtual std::string stringify(); 
-}
+};
 
+#endif
