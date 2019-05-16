b699e1e42eae618ea327a5f4a7aa946452faea1a
Justin Gafford <jgaff001@ucr.edu>
2019-05-16 12:09:22 -0700

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -5,7 +5,7 @@ void SelectionSort::sort(Container* container) {
 	for (unsigned i = 0; i < container->size(); i++) {
 		unsigned index = i;
 		for (unsigned j = i + 1; j < container -> size(); ++j) {
-			if ((*container).at(j)->evaluate() < (*container).at(min)->evaluate()) {
+			if ((*container).at(j)->evaluate() < (*container).at(index)->evaluate()) {
 				index = j;
 			}
 		}
