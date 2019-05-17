32169e9f4abf0ab07b0206c6f5d8ec700c2f741f
Justin Gafford <jgaff001@ucr.edu>
2019-05-17 10:18:44 -0700

    Update Pow.cpp

--- Pow.cpp
+++ Pow.cpp
@@ -7,6 +7,6 @@ using namespace std;
 		return pow(L1 -> evaluate() , L2 -> evaluate());
 	}
 	string Pow::stringify() {
-		return to_string(L1 -> evaluate()) + "**" + to_string(L2 -> evaluate());
+		return to_string(L1 -> stringify() + "**" + L2 -> stringify());
 	}
 
